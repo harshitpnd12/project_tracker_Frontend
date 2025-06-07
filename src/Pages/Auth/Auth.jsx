7e8f932a6c3df0f081c78804ec96a5f77e3312f9
harshitpnd12 <harshit200212pandey@gmail.com>
2025-06-07 18:15:28 +0530

    Fix import casing issue for Signup and Login components

--- src/Pages/Auth/Auth.jsx
+++ src/Pages/Auth/Auth.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Button } from "../../components/ui/button";
 import Login from "./Login";
-import Signup from "./signup";
+import Signup from "./Signup";
 
 const Auth = () => {
   const [active, setActive] = useState(true);
